cb581ea3968d2960aa786f2541c4d263a5309a1b
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-08-27 03:55:48 +0100

    up: code.js

--- code.js
+++ code.js
@@ -1,92 +1,55 @@
 
-const letters = document.querySelectorAll(".letter");
+const ROUNDS = 6;
+const letters = document.querySelectorAll(".box");
+const loadingDiv = document.querySelector(".info-bar");
 
-
+// I like to do an init function so I can use "await"
 async function init() 
 {
+  // the state for the app
   let currentRow = 0;
   let currentGuess = "";
-  let done = false;
-  let isLoading = true;
 
-  // nab the word 
-  const res = await fetch("https://words.dev-apis.com/word-of-the-day");
-  const { word: wordRes } = await res.json();
-  const word = wordRes.toUpperCase();
-  const wordParts = word.split("");
-  isLoading = false;
 
   // user adds a letter to the current guess
   function addLetter(letter) 
   {
-    if (currentGuess.length < ANSWER_LENGTH) {
+    if (currentGuess.length < 5) {
       currentGuess += letter;
     } else {
       currentGuess = currentGuess.substring(0, currentGuess.length - 1) + letter;
     }
 
-    letters[currentRow * ANSWER_LENGTH + currentGuess.length - 1].innerText = letter;
+    letters[currentRow * 5 + currentGuess.length - 1].innerText = letter;
   }
 
-  async function commit() {
-
-    // check the API to see if has a valid word
-    isLoading = true;
-    
-    const res = await fetch("https://words.dev-apis.com/validate-word", {
-      method: "POST",
-      body: JSON.stringify({ word: currentGuess }),
-    });
-    const { validWord } = await res.json();
-    isLoading = false;
-
-    const guessParts = currentGuess.split("");
-
-
-    //  finds correct letters 
-    for (let i = 0; i < ANSWER_LENGTH; i++) {
-      if (guessParts[i] === wordParts[i]) {
-        letters[currentRow * ANSWER_LENGTH + i].classList.add("correct");
-      }
-    }
-    }
-
-    currentRow++;
-    currentGuess = "";
-    
-  }
-
-  function backspace() {
-    currentGuess = currentGuess.substring(0, currentGuess.length - 1);
-    letters[currentRow * ANSWER_LENGTH + currentGuess.length].innerText = "";
-  }
-
- 
-
-  // Listens for keydown events and dispatches actions based on the pressed key.
-  // Ignores events if the task is complete or currently loading
+  // use tries to enter a guess
+  
+  // listening for event keys and routing to the right function
+  // we listen on keydown so we can catch Enter and Backspace
   document.addEventListener("keydown", function handleKeyPress(event) {
-    if (done || isLoading) {
-      // do nothing;
-      return;
-    }
+    
 
-    const action = event.key;
-
-    if (action === "Enter") {
+    if (event.key === "Enter") {
       commit();
-    } else if (action === "Backspace") {
+    } else if (event.key === "Backspace") {
       backspace();
-    } else if (isLetter(action)) {
-      addLetter(action.toUpperCase());
+    } else if (isLetter(event.key)) {
+      addLetter(event.key.toUpperCase());
     } 
   });
-
+}
 
 // check  if a character is alphabet letter
 function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
 }
+
+
+// takes an array of letters (like ['E', 'L', 'I', 'T', 'E']) and creates
+// an object out of it (like {E: 2, L: 1, T: 1}) so we can use that to
+// make sure we get the correct amount of letters marked close instead
+// of just wrong or correct
 
 
 init();
